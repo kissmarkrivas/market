e20bcc3bb27dd5d0b1fc76d0c1d5a7f95aff0604
Kissmark Rivas <rivaskissmark@gmail.com>
2022-09-02 11:51:41 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductoModule } from './producto/producto.module';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ProductoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
